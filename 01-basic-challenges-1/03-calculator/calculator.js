62e425a007417b85a959b5e89184da2a786f0d85
denyschr <denprogman99@gmail.com>
2025-03-06 23:34:42 +0200

    calculator

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,13 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+	const operations = {
+		'+': num1 + num2,
+		'-': num1 - num2,
+		'*': num1 * num2,
+		'/': num1 / num2
+	}
+	return operations[operator]
+		? operations[operator]
+		: (function() { throw new Error("Invalid operator") });
+}
 
 module.exports = calculator;
