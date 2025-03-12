2ef474f5f5229eae0d338d53423b01fd84b0e1a0
denyschr <denprogman99@gmail.com>
2025-03-12 21:53:50 +0200

    palindrome

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,7 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+	const formatted = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+	const reversed = formatted.split('').reverse().join('');
+	return formatted === reversed;
+}
 
 module.exports = isPalindrome;
