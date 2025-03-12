bc6fd2a758c8eeb1a5cada919a87fc2be20f5c7a
denyschr <denprogman99@gmail.com>
2025-03-12 20:19:02 +0200

    title case

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,8 @@
-function titleCase() {}
+function titleCase(str) {
+	return str
+		.split(' ')
+		.map(word => word.charAt(0).toUpperCase() + word.substring(1).toLowerCase())
+		.join(' ');
+}
 
 module.exports = titleCase;
