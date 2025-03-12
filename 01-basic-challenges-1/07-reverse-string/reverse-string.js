e5bb675fc1982a1fb8143ebad988fd74fd31fa10
denyschr <denprogman99@gmail.com>
2025-03-12 21:20:47 +0200

    reverse string

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,11 @@
-function reverseString() {}
+function reverseString(str) {
+	const reversed = str.split('');
+	for (let index = 0; index < reversed.length / 2; index++) {
+		const temp = reversed[index];
+		reversed[index] = reversed[reversed.length - index - 1];
+		reversed[reversed.length - index - 1] = temp;
+	}
+	return reversed.join('');
+}
 
 module.exports = reverseString;
