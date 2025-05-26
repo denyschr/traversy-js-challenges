bf7e762e55218591954570c6e1bd4b2ea8cc6f4c
denyschr <denprogman99@gmail.com>
2025-05-26 16:20:59 +0300

    array intersection

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,5 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+	return [...new Set(arr1.filter((item) => arr2.includes(item)))];
+}
 
 module.exports = arrayIntersection;
