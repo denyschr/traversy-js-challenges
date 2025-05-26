656c5e9c83f64d7a4b6f5edac395b7cd7cdc52b1
denyschr <denprogman99@gmail.com>
2025-05-26 16:07:08 +0300

    fizzbuzz array

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,16 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+	return Array.from({ length: num }, (_, index) => {
+		const n = index + 1;
+		if (n % 3 === 0 && n % 5 === 0) {
+			return 'FizzBuzz';
+		} else if (n % 3 === 0) {
+			return 'Fizz';
+		} else if (n % 5 === 0) {
+			return 'Buzz';
+		} else {
+			return n;
+		}
+	});
+}
 
 module.exports = fizzBuzzArray;
