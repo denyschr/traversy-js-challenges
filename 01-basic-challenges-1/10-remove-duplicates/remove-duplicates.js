1d0b3d6b98bd60cedc96fbd2f870e618345f6a7c
denyschr <denprogman99@gmail.com>
2025-05-26 15:32:48 +0300

    remove duplicates

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,6 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+	const seenItems = new Map();
+	return arr.filter((item) => seenItems.has(item) ? false : seenItems.set(item, true));
+}
 
 module.exports = removeDuplicates;
