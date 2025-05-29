87a7c3ed9121ffb78e22f238bde55c89b99d492b
denyschr <denprogman99@gmail.com>
2025-05-29 18:34:01 +0300

    find missing number

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,11 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+	if (!arr || !arr.length) {
+		return;
+	}
+	const n = arr.length + 1;
+	const expectedSum = n * (n + 1) / 2;
+	const actualSum = arr.reduce((sum, num) => sum + num, 0);
+	return expectedSum - actualSum;
+}
 
 module.exports = findMissingNumber;
