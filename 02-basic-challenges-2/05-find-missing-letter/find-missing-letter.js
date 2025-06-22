37a6b2e439faaa58afc4f05a22859a62158b898e
denyschr <denprogman99@gmail.com>
2025-06-22 19:06:28 +0300

    find missing letter

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,13 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+	const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	const startIndex = alphabet.indexOf(arr[0]);
+	for (let index = 0; index < arr.length; index++) {
+		const expectedLetter = alphabet[startIndex + index];
+		if (arr[index] !== expectedLetter) {
+			return expectedLetter;
+		}
+	}
+	return '';
+}
 
 module.exports = findMissingLetter;
