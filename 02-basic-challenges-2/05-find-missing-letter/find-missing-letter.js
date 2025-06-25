4274701a2229105fe65feee1c8c5a9441dc3cdf5
denyschr <denprogman99@gmail.com>
2025-06-25 22:28:32 +0300

    are all chars unique

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,7 +1,7 @@
 function findMissingLetter(arr) {
 	const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 	const startIndex = alphabet.indexOf(arr[0]);
-	for (let index = 0; index < arr.length; index++) {
+	for (let index = 1; index < arr.length; index++) {
 		const expectedLetter = alphabet[startIndex + index];
 		if (arr[index] !== expectedLetter) {
 			return expectedLetter;
--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,18 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+	if (str === '') {
+		return true;
+	}
+	
+	const uniqueChars = new Set();
+	for (const char of str) {
+		if (uniqueChars.has(char)) {
+			return false;
+		} else {
+			uniqueChars.add(char);
+		}
+	}
+	
+	return true;
+}
 
 module.exports = areAllCharactersUnique;
