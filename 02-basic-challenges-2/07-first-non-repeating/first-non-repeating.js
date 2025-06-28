01ece15e0d75b544edac1c6744749272884d1c81
denyschr <denprogman99@gmail.com>
2025-06-28 23:07:36 +0300

    first non-repeating character

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,17 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+	const charStats = new Map();
+	
+	for (const char of str) {
+		charStats.set(char, (charStats.get(char) || 0) + 1);
+	}
+	
+	for (const [char, count] of charStats.entries()) {
+		if (count === 1) {
+			return char;
+		}
+	}
+	
+	return null;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
