2f84bc72d4d015d53e9a527f5308ec42428721ba
denyschr <denprogman99@gmail.com>
2025-06-29 13:17:00 +0300

    dice game

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,25 @@
-function diceGameSimulation() {}
+function diceGameSimulation(numSimulations) {
+  const rollDice = () => Math.floor(Math.random() * 6) + 1;
+  const winSums = [7, 11];
+  const loseSums = [2, 3, 12];
+
+  return Array.from({ length: numSimulations }, () => {
+    const dice1 = rollDice();
+    const dice2 = rollDice();
+    const sum = dice1 + dice2;
+    const result = winSums.includes(sum)
+      ? 'win'
+      : loseSums.includes(sum)
+      ? 'lose'
+      : 'roll again';
+
+    return {
+      dice1,
+      dice2,
+      sum,
+      result
+    };
+  });
+}
 
 module.exports = diceGameSimulation;
