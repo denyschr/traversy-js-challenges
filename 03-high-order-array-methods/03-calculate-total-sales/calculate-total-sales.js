4f762c9f81de70c6674ffaaeb1eddd7516a6a5f0
denyschr <denprogman99@gmail.com>
2025-07-08 17:56:16 +0300

    calculate total sales

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,9 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, taxRate) {
+  const total = products.reduce(
+    (sum, product) => (sum += product.price * product.quantity),
+    0
+  );
+  return total + (total * taxRate) / 100;
+}
 
 module.exports = calculateTotalSalesWithTax;
