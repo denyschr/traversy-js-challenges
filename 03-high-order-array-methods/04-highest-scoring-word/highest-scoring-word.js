436b0f5e2bceb5e89b62669d26e9719a219116a9
denyschr <denprogman99@gmail.com>
2025-07-08 19:13:24 +0300

    highest scoring word

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,19 @@
-function highestScoringWord() {}
+function highestScoringWord(str) {
+  return str.split(' ').reduce(
+    (highest, word) => {
+      const scores = word
+        .split('')
+        .reduce((sum, char) => (sum += char.charCodeAt(0) - 96), 0);
+
+      if (scores > highest.scores) {
+        highest.word = word;
+        highest.scores = scores;
+      }
+
+      return highest;
+    },
+    { word: null, scores: 0 }
+  ).word;
+}
 
 module.exports = highestScoringWord;
