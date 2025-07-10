fc993ca2e18d1868859b2ad357b718f2697ccefd
denyschr <denprogman99@gmail.com>
2025-07-10 09:59:41 +0300

    valid anagrams

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,8 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+  return (
+    str1.length === str2.length &&
+    str1.split('').sort().join('') === str2.split('').sort().join('')
+  );
+}
 
 module.exports = validAnagrams;
