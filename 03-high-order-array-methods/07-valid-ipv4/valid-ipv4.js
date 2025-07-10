cd9c2a13724641cc8ba8b78ebdfc63378813e409
denyschr <denprogman99@gmail.com>
2025-07-10 12:34:11 +0300

    valid ipv4

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,11 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (input) => {
+  const octets = input.split('.');
+  if (octets.length !== 4) {
+    return false;
+  }
+  return octets.every(
+    (octet) => !/^0\d+$/.test(octet) && +octet >= 0 && +octet <= 255
+  );
+};
 
 module.exports = isValidIPv4;
