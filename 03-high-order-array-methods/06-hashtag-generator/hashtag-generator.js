3d3799bf78b6ba4cc4564545b671912ac8f5f7fb
denyschr <denprogman99@gmail.com>
2025-07-10 11:09:19 +0300

    hashtag generator

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,13 @@
-function generateHashtag() {}
+function generateHashtag(str) {
+  if (str.trim() === '') {
+    return false;
+  }
+
+  const hashtag = `#${str
+    .replace(/\b[a-z]/g, (char) => char.toUpperCase())
+    .replace(/\s+/g, '')}`;
+
+  return hashtag.length > 140 ? false : hashtag;
+}
 
 module.exports = generateHashtag;
