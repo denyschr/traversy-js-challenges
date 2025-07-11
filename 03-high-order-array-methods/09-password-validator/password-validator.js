6845d8b43b6a26fde67bf4905c8c83b4fc84e19e
denyschr <denprogman99@gmail.com>
2025-07-11 16:40:47 +0300

    password validator

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,9 @@
-function validatePassword() {}
+function validatePassword(password) {
+  const hasEnoughLength = password.length >= 8;
+  const hasLowercase = /[a-z]/.test(password);
+  const hasUppercase = /[A-Z]/.test(password);
+  const hasDigit = /[0-9]/.test(password);
+  return hasEnoughLength && hasLowercase && hasUppercase && hasDigit;
+}
 
 module.exports = validatePassword;
