339f0b384d7c3e24e3688e8489c7cbaa7aa20c88
denyschr <denprogman99@gmail.com>
2025-07-12 17:31:56 +0300

    find missing letter refactor

--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,11 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  const missingLetter = arr.find((char, index, theArr) => {
+    const nextChar = theArr[index + 1];
+    return nextChar && nextChar.charCodeAt(0) - char.charCodeAt(0) > 1;
+  });
+  return missingLetter
+    ? String.fromCharCode(missingLetter.charCodeAt(0) + 1)
+    : '';
+}
 
 module.exports = findMissingLetter;
