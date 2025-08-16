bbccbe25bb191583e726f4eafe96912a1819c96e
denyschr <denprogman99@gmail.com>
2025-08-16 17:23:00 +0300

    permutations

--- 04-recursion/10-permutations/permutations.js
+++ 04-recursion/10-permutations/permutations.js
@@ -1,3 +1,21 @@
-function permutations() {}
+function permutations(str) {
+  if (str.length <= 1) {
+    return [str];
+  }
+
+  const result = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    const restOfString = str.slice(0, i) + str.slice(i + 1);
+    const subPermutations = permutations(restOfString);
+
+    for (let j = 0; j < subPermutations.length; j++) {
+      result.push(char + subPermutations[j]);
+    }
+  }
+
+  return result;
+}
 
 module.exports = permutations;
