6908ea1ecc459ce50d4093b19415fc52ef5fd6ee
denyschr <denprogman99@gmail.com>
2025-09-04 17:25:51 +0300

    max subarray quadratic

--- 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
+++ 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
@@ -1,3 +1,19 @@
-function maxSubarraySum() {}
+function maxSubarraySum(arr, k) {
+  let maxSum = -Infinity;
+
+  for (let i = 0; i <= arr.length - k; i++) {
+    let sum = 0;
+
+    for (let j = i; j < i + k; j++) {
+      sum += arr[j];
+    }
+
+    if (sum > maxSum) {
+      maxSum = sum;
+    }
+  }
+
+  return maxSum;
+}
 
 module.exports = maxSubarraySum;
