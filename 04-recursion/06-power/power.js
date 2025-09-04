912945a30ba80e8d8327661a93b4a72e37ad4c94
denyschr <denprogman99@gmail.com>
2025-09-04 13:29:59 +0300

    use more efficient algorithm for recursive power function

--- 04-recursion/06-power/power.js
+++ 04-recursion/06-power/power.js
@@ -2,7 +2,14 @@ function power(base, exponent) {
   if (exponent === 0) {
     return 1;
   }
-  return base * power(base, exponent - 1);
+
+  if (exponent % 2 === 0) {
+    const halfPower = power(base, exponent / 2);
+    return halfPower * halfPower;
+  } else {
+    const halfPower = power(base, (exponent - 1) / 2);
+    return base * halfPower * halfPower;
+  }
 }
 
 module.exports = power;
