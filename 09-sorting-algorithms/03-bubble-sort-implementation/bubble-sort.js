0aad08775a66b5009b47cac1aa29abca21fd989b
denyschr <denprogman99@gmail.com>
2025-09-13 23:13:21 +0300

    bubble sort

--- 09-sorting-algorithms/03-bubble-sort-implementation/bubble-sort.js
+++ 09-sorting-algorithms/03-bubble-sort-implementation/bubble-sort.js
@@ -1,3 +1,15 @@
-function bubbleSort() {}
+function bubbleSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      if (array[j] > [array[j + 1]]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+
+  return array;
+}
 
 module.exports = bubbleSort;
