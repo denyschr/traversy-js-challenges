95b4a5c745e877b03aa6af3f5005dad5a77f1412
denyschr <denprogman99@gmail.com>
2025-09-14 16:34:51 +0300

    merge sort

--- 09-sorting-algorithms/09-merge-sort-implementation/merge-sort.js
+++ 09-sorting-algorithms/09-merge-sort-implementation/merge-sort.js
@@ -1,3 +1,26 @@
-function mergeSort() {}
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const middle = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, middle));
+  const right = mergeSort(arr.slice(middle));
+
+  return merge(left, right);
+}
+
+function merge(left, right) {
+  const merged = [];
+  let leftIndex = (rightIndex = 0);
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    left[leftIndex] < right[rightIndex]
+      ? merged.push(left[leftIndex++])
+      : merged.push(right[rightIndex++]);
+  }
+
+  return [...merged, ...left.slice(leftIndex), ...right.slice(rightIndex)];
+}
 
 module.exports = mergeSort;
