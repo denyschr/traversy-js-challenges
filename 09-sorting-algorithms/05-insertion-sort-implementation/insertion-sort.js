bb125f40271741a76e9a67a6416a25b238697e2a
denyschr <denprogman99@gmail.com>
2025-09-14 09:54:47 +0300

    insertion sort

--- 09-sorting-algorithms/05-insertion-sort-implementation/insertion-sort.js
+++ 09-sorting-algorithms/05-insertion-sort-implementation/insertion-sort.js
@@ -1,3 +1,17 @@
-function insertionSort() {}
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    const temp = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > temp) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+
+    arr[j + 1] = temp;
+  }
+
+  return arr;
+}
 
 module.exports = insertionSort;
