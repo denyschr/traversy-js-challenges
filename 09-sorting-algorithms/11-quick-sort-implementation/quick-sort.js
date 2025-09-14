39ff3bfc776c2ec0ee972fad54c996f2261741de
denyschr <denprogman99@gmail.com>
2025-09-14 18:49:37 +0300

    quick sort

--- 09-sorting-algorithms/11-quick-sort-implementation/quick-sort.js
+++ 09-sorting-algorithms/11-quick-sort-implementation/quick-sort.js
@@ -1,5 +1,18 @@
-function quickSort() {
+function quickSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
 
+  const pivot = arr.at(-1);
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    const element = arr[i];
+    element < pivot ? left.push(element) : right.push(element);
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
 }
 
 module.exports = quickSort;
